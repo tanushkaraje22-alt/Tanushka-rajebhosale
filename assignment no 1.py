9ef451760da12ba31e0c5d0e191364aad065ee91
tanushkaraje22-alt <tanushkaraje22@gmail.com>
2026-03-09 22:47:46 +0530

    Add files via upload

---
+++ assignment no 1.py
@@ -0,0 +1,12 @@
+#create a tuple
+t1=(10,20,30,40)
+print("Tuple:",t1[0])
+print("first element:",t1[0])
+print("second element:",t1[-1])
+
+# nested tuple
+print("2.nested tuple")
+nested_tuple=(1,2,(3,4,5),6)
+print("nested tuple:",nested_tuple)
+print("access nested element:")
+nested_tuple [2][1]
