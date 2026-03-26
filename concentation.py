7d0dcfb1d902f662939c27502069640ee88df8bb
tanushkaraje22-alt <tanushkaraje22@gmail.com>
2026-03-26 19:39:10 +0530

    Add files via upload

---
+++ concentation.py
@@ -0,0 +1,7 @@
+print("4. Concatenation of Tuples")
+t3 = (100, 200)
+t4 = (300, 400)
+concatenated_tuple = t3 + t4
+print("First Tuple:", t3)
+print("Second Tuple:", t4)
+print("Concatenated Tuple:", concatenated_tuple)
